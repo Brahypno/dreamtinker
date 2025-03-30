d04aaf0c0f88e497aed9bc55a57b246b8e6c80b4
Brahypno <alayaalsatia19@gamil.com>
2025-03-30 01:06:18 -0400

    missing commit on tag 1.0;make sure mention caps difference,Man!

--- src/main/java/org/dreamtinker/dreamtinker/Dreamtinker.java
+++ src/main/java/org/dreamtinker/dreamtinker/Dreamtinker.java
@@ -1,12 +1,10 @@
 package org.dreamtinker.dreamtinker;
 
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.dreamtinker.dreamtinker.event.PlayerEvent;
@@ -22,8 +20,8 @@ public class Dreamtinker {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "dreamtinker";
 
-    public Dreamtinker(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
+    public Dreamtinker() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
 
         // Register the commonSetup method for modloading
@@ -52,14 +50,4 @@ public class Dreamtinker {
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
-    }
 }
