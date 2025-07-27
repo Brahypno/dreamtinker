f07b12800d1907bd6d7f2cceb2d71760047170e0
Brahypno <alayaalsatia19@gamil.com>
2025-07-26 22:09:59 -0400

    echo defence

--- src/main/java/org/dreamtinker/dreamtinker/register/DreamtinkerModifer.java
+++ src/main/java/org/dreamtinker/dreamtinker/register/DreamtinkerModifer.java
@@ -6,6 +6,7 @@ import org.dreamtinker.dreamtinker.modifier.Combat.realsweep;
 import org.dreamtinker.dreamtinker.modifier.Combat.silvernamebee;
 import org.dreamtinker.dreamtinker.modifier.Combat.strong_explode;
 import org.dreamtinker.dreamtinker.modifier.material.echo_shard.echoed_attack;
+import org.dreamtinker.dreamtinker.modifier.material.echo_shard.echoed_defence;
 import org.dreamtinker.dreamtinker.modifier.tools.strong_heavy;
 import slimeknights.tconstruct.library.modifiers.util.ModifierDeferredRegister;
 import slimeknights.tconstruct.library.modifiers.util.StaticModifier;
@@ -16,6 +17,7 @@ public class DreamtinkerModifer {
     public static final StaticModifier<realsweep> realsweep = MODIFIERS.register("realsweep", realsweep::new);
     public static final StaticModifier<strong_heavy> strong_heavy = MODIFIERS.register("strong_heavy", strong_heavy::new);
     public static final StaticModifier<echoed_attack> echoed_attack = MODIFIERS.register("echoed_attack", echoed_attack::new);
+    public static final StaticModifier<echoed_defence> echoed_defence = MODIFIERS.register("echoed_defence", echoed_defence::new);
     public static final StaticModifier<strong_explode> strong_explode = MODIFIERS.register("strong_explode", strong_explode::new);
     public static final StaticModifier<silvernamebee> silvernamebee = MODIFIERS.register("silvernamebee", org.dreamtinker.dreamtinker.modifier.Combat.silvernamebee::new);
     public static final StaticModifier<glacialriver> glacial_river = MODIFIERS.register("glacial_river", org.dreamtinker.dreamtinker.modifier.Combat.glacialriver::new);
