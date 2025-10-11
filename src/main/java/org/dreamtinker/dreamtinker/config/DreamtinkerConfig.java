bf075e107f89845b240566d662135d614b183173
Brahypno <alayaalsatia19@gamil.com>
2025-10-10 20:16:23 -0400

    bug fix lupus antimony cannot apply effects properly and neuf default negatives for TheWolfWonder

--- src/main/java/org/dreamtinker/dreamtinker/config/DreamtinkerConfig.java
+++ src/main/java/org/dreamtinker/dreamtinker/config/DreamtinkerConfig.java
@@ -128,7 +128,7 @@ public class DreamtinkerConfig {
     static {builder.comment("\nMetallivorous Stibium Lupus: ");}
 
     public static final ForgeConfigSpec.IntValue TheWolfWonderEffectNum =
-            builder.comment("Number of effective that applied on target").defineInRange("TheWolfWonderEffectNum", 10, 0, 100);
+            builder.comment("Number of effective that applied on target").defineInRange("TheWolfWonderEffectNum", 5, 0, 100);
     public static final ForgeConfigSpec.IntValue TheWolfWonderEffectMinTime =
             builder.comment("Min time effective last in second").defineInRange("TheWolfWonderEffectMinTime", 10, 0, 100);
     public static final ForgeConfigSpec.IntValue TheWolfWonderEffectMaxTime =
