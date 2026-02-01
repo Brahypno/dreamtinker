b983b1c159c66c4a3ba1a979a38baa85738d6379
Brahypno <alayaalsatia19@gamil.com>
2026-01-31 19:33:21 -0500

    Update absorption_defense be one slot

--- src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/armors/absorption_defense.java
+++ src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/armors/absorption_defense.java
@@ -5,24 +5,42 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import org.dreamtinker.dreamtinker.Dreamtinker;
 import org.dreamtinker.dreamtinker.library.modifiers.base.baseclass.ArmorModifier;
+import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.modules.technical.SlotInChargeModule;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
 import static org.dreamtinker.dreamtinker.config.DreamtinkerCachedConfig.AbsorptionDefenseRate;
 
 public class absorption_defense extends ArmorModifier {
+    private static final TinkerDataCapability.TinkerDataKey<SlotInChargeModule.SlotInCharge> SLOT_KEY =
+            TinkerDataCapability.TinkerDataKey.of(Dreamtinker.getLocation("absorption_defense"));
+
+    @Override
+    protected void registerHooks(ModuleHookMap.@NotNull Builder hookBuilder) {
+        hookBuilder.addModule(new SlotInChargeModule(SLOT_KEY));
+        super.registerHooks(hookBuilder);
+    }
+
     @Override
     public float modifyDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
-        float absorption = context.getEntity().getAbsorptionAmount();
-        if (0 < absorption){
-            amount *= (1 - modifier.getLevel() * AbsorptionDefenseRate.get().floatValue());
-            if (absorption < amount && source.getEntity() instanceof LivingEntity entity)
-                entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, modifier.getLevel() * 20, modifier.getLevel()));
-        }else
-            amount *= (1 + modifier.getLevel() * AbsorptionDefenseRate.get().floatValue());
-        
+        int level = SlotInChargeModule.getLevel(context.getTinkerData(), SLOT_KEY, slotType);
+        if (0 < level){
+            float absorption = context.getEntity().getAbsorptionAmount();
+            if (0 < absorption){
+                amount *= (1 - level * AbsorptionDefenseRate.get().floatValue());
+                if (absorption < amount && source.getEntity() instanceof LivingEntity entity)
+                    entity.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, level * 20, level));
+            }else
+                amount *= (1 + level * AbsorptionDefenseRate.get().floatValue());
+        }
+
+
         return amount;
     }
 
