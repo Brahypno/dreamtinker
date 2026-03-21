a248d3101c224adfc548b237d23e79f10bd2ffc0
Brahypno <alayaalsatia19@gamil.com>
2026-03-21 17:15:28 -0400

    better way fix not working

--- src/main/java/org/dreamtinker/dreamtinker/mixin/compact/ars_nouveau/ArmorUpgradeRecipeCategoryMixin.java
+++ src/main/java/org/dreamtinker/dreamtinker/mixin/compact/ars_nouveau/ArmorUpgradeRecipeCategoryMixin.java
@@ -2,24 +2,31 @@ package org.dreamtinker.dreamtinker.mixin.compact.ars_nouveau;
 
 import com.hollingsworth.arsnouveau.api.enchanting_apparatus.ArmorUpgradeRecipe;
 import com.hollingsworth.arsnouveau.client.jei.ArmorUpgradeRecipeCategory;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.recipe.IFocusGroup;
 import net.minecraft.world.item.Item;
-import org.dreamtinker.dreamtinker.tools.items.UnderArmorItem;
+import net.minecraft.world.item.ItemStack;
+import org.dreamtinker.dreamtinker.tools.DreamtinkerTools;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.dreamtinker.dreamtinker.Dreamtinker.configCompactDisabled;
 
 @Mixin(value = ArmorUpgradeRecipeCategory.class, remap = false)
 public class ArmorUpgradeRecipeCategoryMixin {
-    @ModifyReturnValue(
-            method = "lambda$setRecipe$0*",
-            at = @At("RETURN")
-    )
-    private static boolean dt$extendFilter(boolean original, ArmorUpgradeRecipe recipe, Item item) {
-        if (original || !configCompactDisabled("ars_nouveau"))
-            return original;
-
-        return recipe.tier > 0 && item instanceof UnderArmorItem;
+    @ModifyVariable(
+            method = "setRecipe*",
+            at = @At("STORE"),
+            name = "stacks")
+    private List<ItemStack> dt$modifyStacks(List<ItemStack> original, IRecipeLayoutBuilder builder, ArmorUpgradeRecipe recipe, IFocusGroup focuses) {
+        List<ItemStack> mutable = new ArrayList<>(original);
+        // 在这里加你的东西
+        if (0 < recipe.tier && !configCompactDisabled("ars_nouveau"))
+            mutable.addAll(DreamtinkerTools.underPlate.values().stream().map(Item::getDefaultInstance).toList());
+        return mutable;
     }
 }
