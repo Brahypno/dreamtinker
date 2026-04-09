59ba27f6b7ddd77e21d4156f83a0739e94ac3eda
Brahypno <alayaalsatia19@gamil.com>
2026-04-08 22:40:18 -0400

    small improvements

--- src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/armors/ender_dodge.java
+++ src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/armors/ender_dodge.java
@@ -5,9 +5,14 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import org.dreamtinker.dreamtinker.Dreamtinker;
 import org.dreamtinker.dreamtinker.library.modifiers.base.baseclass.ArmorModifier;
 import org.dreamtinker.dreamtinker.utils.DTHelper;
+import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.modules.technical.SlotInChargeModule;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
@@ -15,16 +20,26 @@ import static org.dreamtinker.dreamtinker.config.DreamtinkerConfig.EnderDodgeCha
 import static org.dreamtinker.dreamtinker.config.DreamtinkerConfig.EnderDodgeTimes;
 
 public class ender_dodge extends ArmorModifier {
+    private static final TinkerDataCapability.TinkerDataKey<SlotInChargeModule.SlotInCharge> SLOT_KEY =
+            TinkerDataCapability.TinkerDataKey.of(Dreamtinker.getLocation("ender_dodge"));
 
+    @Override
+    protected void registerHooks(ModuleHookMap.@NotNull Builder hookBuilder) {
+        hookBuilder.addModule(new SlotInChargeModule(SLOT_KEY));
+        super.registerHooks(hookBuilder);
+    }
 
     @Override
     public boolean isDamageBlocked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount) {
-        LivingEntity holder = context.getEntity();
-        Level level = holder.level();
-        if (!level.isClientSide() && !(source.getEntity() instanceof LivingEntity) && holder.level().random.nextFloat() < EnderDodgeChance.get()){
-            for (int i = 0; i < EnderDodgeTimes.get(); ++i) {
-                if (DTHelper.teleport(holder)){
-                    return true;
+        int level = SlotInChargeModule.getLevel(context.getTinkerData(), SLOT_KEY, slotType);
+        if (0 < level){
+            LivingEntity holder = context.getEntity();
+            Level world = holder.level();
+            if (!world.isClientSide() && !(source.getEntity() instanceof LivingEntity) && holder.level().random.nextFloat() < EnderDodgeChance.get()){
+                for (int i = 0; i < EnderDodgeTimes.get() * level; ++i) {
+                    if (DTHelper.teleport(holder)){
+                        return true;
+                    }
                 }
             }
         }
--- src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/material/nigrescence_antimony/ouroboric_hourglass.java
+++ src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/material/nigrescence_antimony/ouroboric_hourglass.java
@@ -2,35 +2,53 @@ package org.dreamtinker.dreamtinker.tools.modifiers.traits.material.nigrescence_
 
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
+import org.dreamtinker.dreamtinker.Dreamtinker;
 import org.dreamtinker.dreamtinker.library.modifiers.base.baseclass.ArmorModifier;
+import org.jetbrains.annotations.NotNull;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.modules.technical.SlotInChargeModule;
+import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.capability.TinkerDataCapability;
 import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
 import static org.dreamtinker.dreamtinker.config.DreamtinkerConfig.OuroboricHourglassMultiply;
 
 public class ouroboric_hourglass extends ArmorModifier {
+    private static final TinkerDataCapability.TinkerDataKey<SlotInChargeModule.SlotInCharge> SLOT_KEY =
+            TinkerDataCapability.TinkerDataKey.of(Dreamtinker.getLocation("absorption_defense"));
 
+    @Override
+    protected void registerHooks(ModuleHookMap.@NotNull Builder hookBuilder) {
+        hookBuilder.addModule(new SlotInChargeModule(SLOT_KEY));
+        super.registerHooks(hookBuilder);
+    }
+
+    @Override
     public float modifyDamageTaken(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount, boolean isDirectDamage) {
-        // 基数：装备者的最大生命值
-        float Base = context.getEntity().getMaxHealth();
-        // 参数
-        float p = 0.8f;
-        Double X = OuroboricHourglassMultiply.get();
-        float final_amount;
+        int level = SlotInChargeModule.getLevel(context.getTinkerData(), SLOT_KEY, slotType);
+        if (0 < level){
+            // 基数：装备者的最大生命值
+            float Base = context.getEntity().getMaxHealth();
+            // 参数
+            float p = 0.8f;
+            Double X = OuroboricHourglassMultiply.get();
+            float final_amount;
 
-        if (amount <= Base){
-            // 小于等于基数：放大段
-            final_amount = Base * (float) Math.pow(amount / Base, p);
-        }else if (amount <= X * Base){
-            // 基数到 X 倍之间：平滑线性减伤
-            float slope = (float) ((float) Math.log(amount / Base) / (X - 1));
-            final_amount = Base + (amount - Base) * slope;
-        }else {
-            // 超过 X 倍：对数压制
-            final_amount = (Base * (float) Math.log(amount / Base / X));
+            if (amount <= Base){
+                // 小于等于基数：放大段
+                final_amount = Base * (float) Math.pow(amount / Base, p);
+            }else if (amount <= X * Base){
+                // 基数到 X 倍之间：平滑线性减伤
+                float slope = (float) ((float) Math.log(amount / Base) / (X - 1));
+                final_amount = Base + (amount - Base) * slope;
+            }else {
+                // 超过 X 倍：对数压制
+                final_amount = (Base * (float) Math.log(amount / Base / X));
+            }
+            return final_amount;
         }
 
-        return final_amount;
+        return amount;
     }
 }
