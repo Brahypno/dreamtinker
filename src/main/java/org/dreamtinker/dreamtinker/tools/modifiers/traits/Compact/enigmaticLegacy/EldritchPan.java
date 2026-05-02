c8d5bcd89ab0e9e854ce14d665c5263add32de99
Brahypno <alayaalsatia19@gmail.com>
2026-05-02 11:31:15 -0400

    stronger EldritchPan

--- src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/Compact/enigmaticLegacy/EldritchPan.java
+++ src/main/java/org/dreamtinker/dreamtinker/tools/modifiers/traits/Compact/enigmaticLegacy/EldritchPan.java
@@ -4,21 +4,34 @@ import com.aizistral.enigmaticlegacy.effects.GrowingBloodlustEffect;
 import com.aizistral.enigmaticlegacy.handlers.SuperpositionHandler;
 import com.aizistral.enigmaticlegacy.helpers.ItemLoreHelper;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.particles.ItemParticleOption;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.LargeFireball;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.dreamtinker.dreamtinker.Dreamtinker;
@@ -28,8 +41,11 @@ import org.jetbrains.annotations.NotNull;
 import slimeknights.mantle.client.TooltipKey;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.modifiers.ModifierHooks;
+import slimeknights.tconstruct.library.modifiers.hook.armor.DamageBlockModifierHook;
 import slimeknights.tconstruct.library.modifiers.modules.build.ModifierTraitModule;
 import slimeknights.tconstruct.library.module.ModuleHookMap;
+import slimeknights.tconstruct.library.tools.context.EquipmentContext;
 import slimeknights.tconstruct.library.tools.context.ToolAttackContext;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 import slimeknights.tconstruct.library.tools.nbt.ModDataNBT;
@@ -42,7 +58,7 @@ import java.util.function.BiConsumer;
 
 import static com.aizistral.enigmaticlegacy.items.EldritchPan.uniqueGainLimit;
 
-public class EldritchPan extends BattleModifier {
+public class EldritchPan extends BattleModifier implements DamageBlockModifierHook {
     public static final ResourceLocation BLOODLUST_ID =
             new ResourceLocation("enigmaticlegacy", "growing_bloodlust");
     public static final ResourceLocation HUNGER_ID =
@@ -50,6 +66,7 @@ public class EldritchPan extends BattleModifier {
 
     @Override
     protected void registerHooks(ModuleHookMap.@NotNull Builder hookBuilder) {
+        hookBuilder.addHook(this, ModifierHooks.DAMAGE_BLOCK);
         hookBuilder.addModule(new ModifierTraitModule(DreamtinkerModifiers.cursed_ring_bound.getId(), 1, true));
         hookBuilder.addModule(new ModifierTraitModule(TinkerModifiers.blocking.getId(), 1, true));
         super.registerHooks(hookBuilder);
@@ -65,6 +82,57 @@ public class EldritchPan extends BattleModifier {
     public Component validate(IToolStackView tool, ModifierEntry modifier) {
         tool.getPersistentData().putInt(CursedRingBound.TAG_DEEP_CURSE, 1);
         return null;
+    }
+
+    @Override
+    public boolean isDamageBlocked(IToolStackView tool, ModifierEntry modifier, EquipmentContext context, EquipmentSlot slotType, DamageSource source, float amount) {
+        LivingEntity entity = context.getEntity();
+        if (entity.isBlocking() && entity instanceof Player player){
+            Entity projectile = source.getDirectEntity();
+
+            if (!(projectile instanceof Projectile))
+                return false;
+
+            if (!source.is(DamageTypeTags.BYPASSES_SHIELD)){
+                Vec3 sourcePos = source.getSourcePosition();
+
+                if (sourcePos != null){
+                    Vec3 lookVec = entity.getViewVector(1.0F);
+                    Vec3 sourceToSelf = sourcePos.vectorTo(entity.position()).normalize();
+                    sourceToSelf = new Vec3(sourceToSelf.x, 0.0D, sourceToSelf.z);
+
+                    if (sourceToSelf.dot(lookVec) < 0.0D){
+                        projectile.kill();
+
+                        FoodData data = player.getFoodData();
+                        data.eat(4, 0.5F);
+
+                        entity.level().playSound(null, player.getX(), player.getY(), player.getZ(),
+                                                 SoundEvents.PLAYER_BURP, SoundSource.PLAYERS, 0.5F,
+                                                 player.level().random.nextFloat() * 0.1F + 0.9F);
+
+                        player.gameEvent(GameEvent.EAT);
+
+                        if (projectile instanceof LargeFireball fireball){
+                            fireball.explosionPower = 0;
+                        }
+
+                        if (player.level() instanceof ServerLevel level){
+                            Vec3 angle = player.getLookAngle();
+                            angle.multiply(1, 0, 1).normalize().multiply(0.5, 0.5, 0.5);
+
+                            level.sendParticles(new ItemParticleOption(ParticleTypes.ITEM,
+                                                                       new ItemStack(Items.FIRE_CHARGE)), player.getX() + angle.x,
+                                                player.getY() + player.getEyeHeight() - 0.1, player.getZ() + angle.z,
+                                                10, 0.3D, 0.3D, 0.3D, 0.03D);
+                        }
+
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     public static final ResourceLocation TAG_PAN = new ResourceLocation(Dreamtinker.MODID, "eldritch_pan");
