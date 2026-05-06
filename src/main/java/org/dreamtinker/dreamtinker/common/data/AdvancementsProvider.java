b21e2843cf03ebdd9f9d4abf61c7f2382e5dc334
Brahypno <alayaalsatia19@gmail.com>
2026-05-05 21:40:16 -0400

    advancement updates

--- src/main/java/org/dreamtinker/dreamtinker/common/data/AdvancementsProvider.java
+++ src/main/java/org/dreamtinker/dreamtinker/common/data/AdvancementsProvider.java
@@ -4,8 +4,7 @@ import com.google.common.collect.Sets;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.CriterionTriggerInstance;
 import net.minecraft.advancements.FrameType;
-import net.minecraft.advancements.critereon.InventoryChangeTrigger;
-import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.advancements.critereon.*;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.PackOutput;
 import net.minecraft.nbt.CompoundTag;
@@ -13,10 +12,11 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.ConditionalAdvancement;
 import org.dreamtinker.dreamtinker.Dreamtinker;
+import org.dreamtinker.dreamtinker.common.DreamtinkerCommon;
+import org.dreamtinker.dreamtinker.fluids.DreamtinkerFluids;
 import org.dreamtinker.dreamtinker.tools.DreamtinkerTools;
 import org.dreamtinker.dreamtinker.tools.data.DreamtinkerMaterialIds;
 import org.jetbrains.annotations.NotNull;
@@ -26,9 +26,13 @@ import slimeknights.tconstruct.library.json.predicate.tool.HasMaterialPredicate;
 import slimeknights.tconstruct.library.json.predicate.tool.StatInRangePredicate;
 import slimeknights.tconstruct.library.json.predicate.tool.ToolStackItemPredicate;
 import slimeknights.tconstruct.library.json.predicate.tool.ToolStackPredicate;
+import slimeknights.tconstruct.library.materials.definition.MaterialVariant;
+import slimeknights.tconstruct.library.tools.helper.ToolBuildHandler;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
+import slimeknights.tconstruct.library.tools.nbt.MaterialNBT;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
 import slimeknights.tconstruct.tools.TinkerToolParts;
+import slimeknights.tconstruct.tools.data.material.MaterialIds;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -55,24 +59,233 @@ public class AdvancementsProvider extends GenericDataProvider {
     }
 
     protected void generate() {
+        Advancement dreamtinkerRoot = builderSilentRoot(
+                DreamtinkerCommon.hypnagogic_transmute.get(),
+                resource("root"),
+                resource("textures/block/transmute/ashen/ashen_road_back.png"),
+                FrameType.TASK,
+                builder -> builder.addCriterion("tick", PlayerTrigger.TriggerInstance.tick())
+        );
 
+
+        //tool
         Advancement getSilenceGlove =
-                builder(DreamtinkerTools.silence_glove, resource("tools/silence_glove"), resource("textures/gui/advancement_background.png"), FrameType.TASK,
-                        builder ->
-                                builder.addCriterion("crafted_book", hasItem(DreamtinkerTools.silence_glove)));
-        IJsonPredicate<IToolStackView> six_hand_predicate = ToolStackPredicate.and(StatInRangePredicate.min(ToolStats.ATTACK_DAMAGE, 9f),
-                                                                                   ToolStackPredicate.set(DreamtinkerTools.silence_glove.get()));
-        builder(Items.ZOMBIE_HEAD, resource("tools/six_finger"), getSilenceGlove, FrameType.GOAL, builder ->
-                builder.addCriterion("damage", InventoryChangeTrigger.TriggerInstance.hasItems(ToolStackItemPredicate.ofTool(six_hand_predicate))));
+                builder(DreamtinkerTools.silence_glove.get().getRenderTool(),
+                        resource("tools/silence_glove"),
+                        dreamtinkerRoot,
+                        FrameType.TASK,
+                        builder -> builder.addCriterion(
+                                "crafted_silence_glove",
+                                hasItem(DreamtinkerTools.silence_glove)
+                        ));
 
-        CompoundTag nbt = new CompoundTag();
-        nbt.putString("Material", DreamtinkerMaterialIds.RuinWheelSteel.toString());
+        IJsonPredicate<IToolStackView> sixHandPredicate = ToolStackPredicate.and(
+                StatInRangePredicate.min(ToolStats.ATTACK_DAMAGE, 9f),
+                ToolStackPredicate.set(DreamtinkerTools.silence_glove.get())
+        );
+
+        MaterialNBT nbt = MaterialNBT.of(MaterialVariant.of(MaterialIds.iron, ""), MaterialVariant.of(DreamtinkerMaterialIds.black_sapphire, ""),
+                                         MaterialVariant.of(DreamtinkerMaterialIds.shadowskin, ""));
+        ItemStack silence_glove = ToolBuildHandler.buildItemFromMaterials(DreamtinkerTools.silence_glove.get(), nbt);
+        builder(silence_glove,
+                resource("tools/six_finger"),
+                getSilenceGlove,
+                FrameType.GOAL,
+                builder -> builder.addCriterion(
+                        "damage",
+                        InventoryChangeTrigger.TriggerInstance.hasItems(
+                                ToolStackItemPredicate.ofTool(sixHandPredicate)
+                        )
+                ));
+
+        /*
+         * Materials line:
+         * raw_stibnite -> valentinite -> nigrescence_antimony
+         * -> metallivorous_stibium_lupus -> star_regulus
+         */
+
+        Advancement rawStibnite =
+                builder(DreamtinkerCommon.raw_stibnite.get(),
+                        resource("materials/raw_stibnite"),
+                        dreamtinkerRoot,
+                        FrameType.TASK,
+                        builder -> builder.addCriterion(
+                                "has_raw_stibnite",
+                                hasItem(DreamtinkerCommon.raw_stibnite.get())
+                        ));
+
+        Advancement valentinite =
+                builder(DreamtinkerCommon.valentinite.get(),
+                        resource("materials/valentinite"),
+                        rawStibnite,
+                        FrameType.TASK,
+                        builder -> builder.addCriterion(
+                                "has_valentinite",
+                                hasItem(DreamtinkerCommon.valentinite.get())
+                        ));
+
+        Advancement nigrescenceAntimony =
+                builder(DreamtinkerCommon.nigrescence_antimony.get(),
+                        resource("materials/nigrescence_antimony"),
+                        valentinite,
+                        FrameType.GOAL,
+                        builder -> builder.addCriterion(
+                                "has_nigrescence_antimony",
+                                hasItem(DreamtinkerCommon.nigrescence_antimony.get())
+                        ));
+        Advancement moltenAlbedoStibium =
+                builder(DreamtinkerFluids.molten_albedo_stibium.getBucket(),
+                        resource("materials/molten_albedo_stibium"),
+                        nigrescenceAntimony,
+                        FrameType.GOAL,
+                        builder -> builder.addCriterion(
+                                "has_molten_albedo_stibium",
+                                hasItem(DreamtinkerFluids.molten_albedo_stibium.getBucket())
+                        ));
+
+        Advancement metallivorousStibiumLupus =
+                builder(DreamtinkerCommon.metallivorous_stibium_lupus.get(),
+                        resource("materials/metallivorous_stibium_lupus"),
+                        moltenAlbedoStibium,
+                        FrameType.GOAL,
+                        builder -> builder.addCriterion(
+                                "has_metallivorous_stibium_lupus",
+                                hasItem(DreamtinkerCommon.metallivorous_stibium_lupus.get())
+                        ));
+        Advancement moltenAscendingAntimony =
+                builder(DreamtinkerFluids.molten_ascending_antimony.getBucket(),
+                        resource("materials/molten_ascending_antimony"),
+                        metallivorousStibiumLupus,
+                        FrameType.GOAL,
+                        builder -> builder.addCriterion(
+                                "has_molten_ascending_antimony",
+                                hasItem(DreamtinkerFluids.molten_ascending_antimony.getBucket())
+                        ));
+
+        Advancement liquidSmokyAntimony =
+                builder(DreamtinkerFluids.liquid_smoky_antimony.getBucket(),
+                        resource("materials/liquid_smoky_antimony"),
+                        moltenAscendingAntimony,
+                        FrameType.GOAL,
+                        builder -> builder.addCriterion(
+                                "has_liquid_smoky_antimony",
+                                hasItem(DreamtinkerFluids.liquid_smoky_antimony.getBucket())
+                        ));
+
+        Advancement starRegulus =
+                builder(DreamtinkerCommon.regulus.get(),
+                        resource("materials/star_regulus"),
+                        liquidSmokyAntimony,
+                        FrameType.CHALLENGE,
+                        builder -> builder.addCriterion(
+                                "has_star_regulus",
+                                hasItem(DreamtinkerCommon.regulus.get())
+                        ));
+        builderHidden(
+                new ItemStack(DreamtinkerCommon.regulus.get()),
+                resource("materials/magnum_opus"),
+                starRegulus,
+                FrameType.TASK,
+                builder -> builder.addCriterion(
+                        "star_regulus",
+                        new UsingItemTrigger.TriggerInstance(
+                                ContextAwarePredicate.ANY,
+                                ItemPredicate.Builder.item()
+                                                     .of(DreamtinkerCommon.regulus.get())
+                                                     .build()
+                        )
+                )
+        );
+        Advancement orichalcumIngot =
+                builder(DreamtinkerCommon.orichalcum.get(),
+                        resource("materials/orichalcum_ingot"),
+                        dreamtinkerRoot,
+                        FrameType.TASK,
+                        builder -> builder.addCriterion(
+                                "has_orichalcum_ingot",
+                                hasItem(DreamtinkerCommon.orichalcum.get())
+                        ));
+
+        Advancement coldIronIngot =
+                builder(DreamtinkerCommon.cold_iron_ingot.get(),
+                        resource("materials/cold_iron_ingot"),
+                        dreamtinkerRoot,
+                        FrameType.TASK,
+                        builder -> builder.addCriterion(
+                                "has_cold_iron_ingot",
+                                hasItem(DreamtinkerCommon.cold_iron_ingot.get())
+                        ));
+
+        Advancement transmutationGoldIngot =
+                builder(DreamtinkerCommon.transmutation_gold_ingot.get(),
+                        resource("materials/transmutation_gold_ingot"),
+                        dreamtinkerRoot,
+                        FrameType.TASK,
+                        builder -> builder.addCriterion(
+                                "has_transmutation_gold_ingot",
+                                hasItem(DreamtinkerCommon.transmutation_gold_ingot.get())
+                        ));
+
+        builderHidden(
+                new ItemStack(DreamtinkerCommon.shadow_silver_ingot.get()),
+                resource("materials/shadow_silver_ingot"),
+                dreamtinkerRoot,
+                FrameType.TASK,
+                builder -> builder.addCriterion(
+                        "has_shadow_silver_ingot",
+                        hasItem(DreamtinkerCommon.shadow_silver_ingot.get())
+                )
+        );
+
+        /*
+         * Ruin Wheel Steel material icon
+         */
+
+        CompoundTag ruinWheelSteelNbt = new CompoundTag();
+        ruinWheelSteelNbt.putString("Material", DreamtinkerMaterialIds.RuinWheelSteel.toString());
+
         ItemStack ruinSteel = new ItemStack(TinkerToolParts.fakeIngot.get());
-        ruinSteel.setTag(nbt);
-        builder(ruinSteel, resource("tools/ruin_wheel"), resource("textures/gui/advancement_background.png"),
-                FrameType.CHALLENGE, builder ->
-                        builder.addCriterion("material", InventoryChangeTrigger.TriggerInstance.hasItems(
-                                ToolStackItemPredicate.ofContext(new HasMaterialPredicate(DreamtinkerMaterialIds.RuinWheelSteel)))));
+        ruinSteel.setTag(ruinWheelSteelNbt);
+
+        builderHidden(
+                ruinSteel,
+                resource("materials/ruin_wheel"),
+                dreamtinkerRoot,
+                FrameType.CHALLENGE,
+                builder -> builder.addCriterion(
+                        "has_ruin_wheel_steel_material",
+                        InventoryChangeTrigger.TriggerInstance.hasItems(
+                                ToolStackItemPredicate.ofContext(
+                                        new HasMaterialPredicate(DreamtinkerMaterialIds.RuinWheelSteel)
+                                )
+                        )
+                )
+        );
+    }
+
+    private Advancement builderSilentRoot(
+            ItemLike icon,
+            ResourceLocation id,
+            ResourceLocation background,
+            FrameType frame,
+            Consumer<Advancement.Builder> consumer
+    ) {
+        Advancement.Builder builder = Advancement.Builder.advancement();
+
+        builder.display(
+                new ItemStack(icon),
+                Component.translatable("advancements." + id.getNamespace() + "." + id.getPath().replace('/', '.') + ".title"),
+                Component.translatable("advancements." + id.getNamespace() + "." + id.getPath().replace('/', '.') + ".description"),
+                background,
+                frame,
+                false, // showToast
+                false, // announceToChat
+                false  // hidden
+        );
+
+        consumer.accept(builder);
+
+        return builder.save(advancementConsumer, id.toString());
     }
 
     @Override
@@ -139,6 +352,33 @@ public class AdvancementsProvider extends GenericDataProvider {
 
     protected ResourceLocation resource(String name) {
         return Dreamtinker.getLocation(name);
+    }
+
+    private Advancement builderHidden(
+            ItemStack icon,
+            ResourceLocation id,
+            Advancement parent,
+            FrameType frame,
+            Consumer<Advancement.Builder> consumer
+    ) {
+        Advancement.Builder builder = Advancement.Builder.advancement();
+
+        builder.parent(parent);
+
+        builder.display(
+                icon,
+                Component.translatable("advancements." + id.getNamespace() + "." + id.getPath().replace('/', '.') + ".title"),
+                Component.translatable("advancements." + id.getNamespace() + "." + id.getPath().replace('/', '.') + ".description"),
+                null,
+                frame,
+                true,   // showToast
+                true,   // announceToChat
+                true    // hidden
+        );
+
+        consumer.accept(builder);
+
+        return builder.save(advancementConsumer, id.toString());
     }
 
     @Override
